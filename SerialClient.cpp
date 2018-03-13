0141f7040e6166b284dfcfce4dabe4b78470a6d4
Unchapped <nate@nathanielchapman.com>
2018-03-12 19:46:10 -0700

    started updating the serial parser

--- SerialClient.cpp
+++ SerialClient.cpp
@@ -1,9 +1,32 @@
 #include "SerialClient.h"
 
+#define CHAR_NOMS -2
+#define CHAR_ERR -1
+
+
 SerialClientClass::SerialClientClass(HardwareSerial & port) : _status_rate(SERIAL_STATUS_MILLIS), _port(port) {}
+
+static int parse_char(char in) {
+    static uint8_t _num_ptr = 0;
+    static char    _num_buf[4]; //255 max
+    if(in > '0' && in <= '9') { //valid number
+    	_num_buf[_num_ptr++] = in;
+			if(4 == _num_ptr) {
+				_num_buf[_num_ptr++] = 0;
+				_num_ptr = 0;
+				return CHAR_ERR;
+			}
+			return CHAR_NOMS;
+    } else { //another ascii character encountered, return the value read
+    	_num_buf[_num_ptr++] = 0;
+			_num_ptr = 0;
+			return atoi(_num_buf);
+    }
+}
 
 void SerialClientClass::init(unsigned int speed ){
 	_port.begin(speed);
+	_port.println("Leduino Online, H = on, L = off, chan:val");
 }
 
 void SerialClientClass::poll(){
@@ -19,6 +42,48 @@ void SerialClientClass::poll(){
 		_port.println();
 	}
 	//TODO: define serial contol protocol and parser here!
+
+	if (_port.available() > 0) {
+    char in = _port.read();
+    if (in == 'H') {
+    	LedController.power(true);
+      _serial_state = SERIAL_STATE_ERR;
+    } else if (in == 'L') {
+    	LedController.power(false);
+      _serial_state = SERIAL_STATE_ERR;
+    } else {
+    	int res = parse_char(in);
+    	switch (res){
+    		case CHAR_NOMS:
+    			return;
+    		break;
+    		case CHAR_ERR:
+    			_serial_state = SERIAL_STATE_ERR;
+    		break;
+    		default:
+    			switch(_serial_state){
+    				case SERIAL_STATE_CHANNEL:
+    					_channel = res;
+    				break;
+    				case SERIAL_STATE_VALUE:
+    					LedController.queueChannel(_channel, res, 1);
+    				break;
+    				default: //SERIAL_STATE_ERROR
+    				switch(in){
+		          case ',':
+		          case ' ':
+		          case 0x0A: //new line
+		          case 0x11: //tab
+		            _serial_state = SERIAL_STATE_CHANNEL;
+		          break;
+		          default: //just consume input till you reach a space
+		          break;
+		        }
+    			}
+    		break;
+    	}
+    }
+  }
 }
 
 //Static Instance
--- SerialClient.h
+++ SerialClient.h
@@ -10,9 +10,16 @@
 //How often to dump channel settings to the serial port
 #define SERIAL_STATUS_MILLIS 500
 
+#define SERIAL_STATE_CHANNEL 0
+#define SERIAL_STATE_VALUE 1
+//#define SERIAL_STATE_DELAY 2 Just use 1 second delay for now
+#define SERIAL_STATE_ERR 3
+
 class SerialClientClass{
     HardwareSerial & _port;
     FancyDelay _status_rate; //1 Hz report rate
+    uint8_t _serial_state = SERIAL_STATE_CHANNEL;
+    uint8_t _channel = 0;
 
     public:
     SerialClientClass(HardwareSerial & port);
